b7f86ac69803f0563c28fffb90e486252678708f
Avinash Mudunuri <avinashmudunuri@gmail.com>
2017-11-26 22:16:53 +0530

    updated messages and key

--- js/main.js
+++ js/main.js
@@ -199,6 +199,7 @@ jQuery(function($) {'use strict';
 		var form_status = $('<div class="form_status"></div>');
 		$.ajax({
 			url: $(this).attr('action'),
+			type: "POST",
 			beforeSend: function(){
 				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
 			}
